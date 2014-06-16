a8ae3746653c08638ee228cb9bbbfdc310a8f226
Jake Harris <milieu@javakat.io>
2014-06-16 11:40:35 -0500

    Fix twitter url loader

--- dev/js/controllers.js
+++ dev/js/controllers.js
@@ -28,13 +28,24 @@ home.controller('twitter-ctrl', function ($scope, $http) {
   // Named massage because we are getting the data into a nicer format for the page
   var massage = function (tweet) {
     var m = tweet.text
-      , link = tweet.entities.urls[0].expanded_url
+      , link = ''
       , d = new Date(tweet.created_at.split('+0000').join(''))
       , mentions = tweet.user_mentions
       
       //TODO: make this have multiplicity, i.e. it can handle many links per tweet
       
-      if(~m.indexOf(link)) {
+      if (tweet.entities.urls.length > 0)
+        link = tweet.entities.urls[0].expanded_url
+      else if (tweet.entities.media.length > 0)
+        link = tweet.entities.media[0].expanded_url
+      else if (/(http:\/\/\S*){1}?/.test(m)) {
+        //yes I could write this in fewer lines but I'm trying to help you read it
+        var match = /(http:\/\/\S*){1}?/.exec(m)
+        link = match[0]  
+      }
+      else link = ''
+    
+      if(link != '' && ~m.indexOf(link)) {
           m = m.replace(link, '')
       }
       while(~m.indexOf('&amp;'))
