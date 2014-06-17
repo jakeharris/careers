881dacf6bb964a26b968f2bd530bc4234dd09677
Jake Harris <milieu@javakat.io>
2014-06-17 11:53:53 -0500

    Ensure only two events get loaded

--- dev/js/controllers.js
+++ dev/js/controllers.js
@@ -17,7 +17,7 @@ var home = angular.module('career-center-home', [])
 home.controller('calendar-ctrl', function ($scope, $http) {
   $http.get('./events.json')
        .then(function (res) {
-         $scope.events = res.data
+         $scope.events = res.data.slice(0, 2)
        })
 })
 
