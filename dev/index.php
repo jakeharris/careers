81f15377f2c1f423b4e95227ca87cb63c7b72171
Jake Harris <milieu@javakat.io>
2014-08-26 11:28:06 -0500

    Tiny modifications at Nancy's request

--- dev/index.php
+++ dev/index.php
@@ -194,8 +194,8 @@
                             <div class="times">
                                 <i class="fa fa-clock-o times-icon"></i>
                                 <div class="times-address">
-                                    Hours: Mon-Fri 7:45am-4:45pm <br>
-                                    Walk-ins: MRF 12:00n-4pm | TW 9:00am-4:00pm
+                                    Hours: <br /> Mon-Fri 7:45am-4:45pm <br />
+                                    Walk-ins: <br />Mon, Thurs, Fri 12:00 noon-4pm <br /> Tues, Wed 9:00am-4:00pm
                                     <p class="subtle">
                                         15 minute sessions for resume and cover 
                                         letter reviews and basic job search questions.
