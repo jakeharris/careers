73dcea77df391fe608f3d3a2dea9835d35ddfcc6
Jake Harris <milieu@javakat.io>
2014-09-18 13:26:54 -0500

    Ask events page to sort

--- events/assets/js/controllers.js
+++ events/assets/js/controllers.js
@@ -3,6 +3,9 @@ var events = angular.module('career-center-events', [])
 events.controller('calendar-ctrl', function ($scope, $http) {
   $http.get('../events.json')
        .then(function (res) {
-         $scope.events = res.data
+         $scope.events = res.data.sort(function (a, b) {
+            if(a.date['numerical-month'] == b.date['numerical-month']) return a.date['day'] - b.date['day']
+            return a.date['numerical-month'] - b.date['numerical-month']
+         })
        })
 })
