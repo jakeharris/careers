d3d9b5cb92c94bbbd8dc48e8de37d22507bd1048
Jake Harris <milieu@javakat.io>
2014-11-11 11:20:00 -0600

    Remove POP event from carousel

--- dev/index.php
+++ dev/index.php
@@ -113,16 +113,12 @@
                         <ol class="carousel-indicators">
                             <li data-target="#cc-carousel" data-slide-to="0" class="active"></li>
                             <li data-target="#cc-carousel" data-slide-to="1"></li>
-                            <li data-target="#cc-carousel" data-slide-to="2"></li>
                         </ol>
 
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner">
                             <div class="item active">
                                 <img src="assets/images/Comm&Med_Homepage.png" alt="...">
-                            </div>
-                            <div class="item">
-                                <img src="assets/images/POP_Homepage.png" alt="...">
                             </div>
                             <div class="item">
                                 <img src="assets/images/Welcome_Homepage.png" alt="...">
