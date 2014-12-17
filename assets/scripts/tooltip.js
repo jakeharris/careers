18beec14fa0bb3ef686c4d19e512c9981be80d22
Jake <milieu@javakat.io>
2014-12-17 14:55:58 -0600

    Finish tooltip.js v1.0

--- assets/scripts/tooltip.js
+++ assets/scripts/tooltip.js
@@ -2,11 +2,21 @@
   document.addEventListener('DOMContentLoaded', function (event) {
     console.log($('[data-tooltip]'))
     console.log($('[data-for=' + $('[data-tooltip]').attr('data-tooltip') + ']'))
-    
-    var wedges = $('.link-wedges'),
-        firstTooltips = $(wedges.children('[data-tooltip]')[0])
-    
-    console.log(firstTooltips.attr('data-tooltip'))
-    $('[data-for=' + firstTooltips.attr('data-tooltip') + ']').toggleClass('tooltip--show')
+
+    var wedgeContainers = $('.link-wedges'),
+        wedges = $(wedgeContainers.children('.wedge')),
+        tooltips = $($('.tooltips').children('.tooltip')),
+        firstWedges = $(wedgeContainers.children('[data-tooltip]:first-child')),
+        firstTooltips = $($('.tooltips').children('.tooltip[data-for]:first-child'))
+
+    firstWedges.addClass('wedge--hovered')
+    firstTooltips.addClass('tooltip--show')
+
+    wedges.hover(function () {
+      $($(this).parent().children('.wedge')).filter('.wedge--hovered').removeClass('wedge--hovered')
+      $(this).addClass('wedge--hovered')
+      $($(this).parent().siblings('.tooltips').children('.tooltip')).filter('.tooltip--show').removeClass('tooltip--show')
+      $('[data-for=' + $(this).data('tooltip') + ']').addClass('tooltip--show')
+    })
   })
 })()
