90f9f8fd16e6e99b3558c73d343b9af124d710e9
Jake Harris <milieu@javakat.io>
2015-07-27 13:42:00 -0500

    Copy about-us pages to employer side, fixes #286
    There should be an easier way to do this than how I am.

---
+++ assets/scripts/employer-about-us.js
@@ -0,0 +1,24 @@
+var home = angular.module('career-center-about-us', ['ngRoute'])
+
+home.config(['$interpolateProvider', function ($interpolateProvider) {
+  'use strict';
+  $interpolateProvider.startSymbol('[[')
+  $interpolateProvider.endSymbol(']]')
+}]);
+
+home.controller('hours-ctrl', ['$scope', '$http', function ($scope, $http) {
+  'use strict';
+  
+  var hoursFile = (window.location.href.indexOf('localhost') != -1) ?
+                    'http://auburn.edu/career/hours.json' : '/career/hours.json'
+  
+  $http.get(hoursFile)
+  .then(function (res) {
+    if(res.data.hasOwnProperty('holiday'))
+      $scope.holiday = res.data.holiday
+    if(res.data.hasOwnProperty('types')) {
+      $scope.office = res.data.types.office
+      $scope.walkIn = res.data.types.walkIn
+    }
+  })
+}])
