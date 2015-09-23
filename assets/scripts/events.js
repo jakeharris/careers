c7e023b9c95b14dd1cd6c407285b729925604588
Jake Harris <milieu@javakat.io>
2015-09-23 12:18:59 -0500

    Enable static mode on month loader, only load eventsFile once

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,5 +1,7 @@
 var events = angular.module('career-center-events', ['ngRoute']),
-    employerMode = (window.location.href.indexOf('employers') != -1 || window.location.href.indexOf('hire.auburn.edu') != -1)
+    employerMode = (window.location.href.indexOf('employers') != -1 || window.location.href.indexOf('hire.auburn.edu') != -1),
+    eventsFile = (window.location.href.indexOf('localhost') != -1) ? 
+                    'http://auburn.edu/career/events.json' : '/career/events.json'
 
 events.config(['$interpolateProvider', function($interpolateProvider) {
   'use strict';
@@ -10,26 +12,22 @@ events.config(['$interpolateProvider', function($interpolateProvider) {
 events.controller('month-ctrl', ['$scope', '$http', function ($scope, $http) {
   'use strict';
   
-  var eventsFile = (window.location.href.indexOf('localhost') != -1) ? 
-                    'http://auburn.edu/career/events.json' : '/career/events.json',
-      calendar
+  var calendar
   
   $http.get(eventsFile)
   .then(function (res) {
-    calendar = new Calendar(res, 0, employerMode)
+    calendar = new Calendar(res, 0, employerMode, true)
     $scope.events = calendar.events.slice(0, 6)
   })
 }])
 
 events.controller('calendar-ctrl', ['$scope', '$http', function ($scope, $http) {
   'use strict';
-  var eventsFile = (window.location.href.indexOf('localhost') != -1) ? 
-                    'http://auburn.edu/career/events.json' : '/career/events.json',
-      calendar
+  var calendar
   
   $http.get(eventsFile)
   .then(function (res) {
-    calendar = new Calendar(res, 6, employerMode)
+    calendar = new Calendar(res, 6, employerMode, false)
     $scope.events = calendar.events
   })
 }])
