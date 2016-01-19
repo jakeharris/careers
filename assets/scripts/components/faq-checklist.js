6077c89b556f402129f437f7adc9d5e1ed97f10c
Jake Harris <milieu@javakat.io>
2016-01-19 15:55:57 -0600

    Quick commit

--- assets/scripts/components/faq-checklist.js
+++ assets/scripts/components/faq-checklist.js
@@ -1,9 +1,8 @@
 $(function () {
-  var items = $('.faq-checklist-item')
+  var items = $('li.hidden')
 
-  $(items[0]).removeClass('hidden')
   items.each(function (i, e) {
-    $($(this).children('.switch')[0]).children('input').on('change', function () {
+    $(this).children('.switch').first().children('input').on('change', function () {
       var conditionalResults = $(this).parent().siblings('.switch-result')
 
       console.log(this)
@@ -18,4 +17,22 @@ $(function () {
         $(items[i+1]).removeClass('hidden')
     })
   })
+  
+  items.first().removeClass('hidden')
+  
+  var firstSwitch = items.first().children('.switch').first(),
+      conditionalResults = firstSwitch.siblings('.switch-result'),
+      yes = firstSwitch.children('input:checked#*-yes'),
+      no = firstSwitch.children('input:checked#*-no')
+
+  if(yes)
+    $(conditionalResults[0]).show()
+    $(conditionalResults[1]).hide()
+  } else if (no) {
+    $(conditionalResults[0]).hide()
+    $(conditionalResults[1]).show()
+  }
+  if($(items[i+1]).hasClass('hidden'))
+    $(items[i+1]).removeClass('hidden')
+    
 })
