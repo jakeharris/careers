98a1173bc9f7e867be8384e3b6699341fca0136b
Jake Harris <milieu@javakat.io>
2016-01-20 11:55:09 -0600

    Thoroughly refactor Internships page (work towards #567)

--- assets/scripts/components/faq-checklist.js
+++ assets/scripts/components/faq-checklist.js
@@ -1,5 +1,5 @@
 $(function () {
-  var items = $('li.hidden')
+  var items = $('.checklist li.hidden')
 
   items.each(function (i, e) {
     $(this).children('.switch').first().children('input').on('change', function () {
@@ -20,19 +20,33 @@ $(function () {
   
   items.first().removeClass('hidden')
   
-  var firstSwitch = items.first().children('.switch').first(),
-      conditionalResults = firstSwitch.siblings('.switch-result'),
-      yes = firstSwitch.children('input:checked#*-yes'),
-      no = firstSwitch.children('input:checked#*-no')
-
-  if(yes)
-    $(conditionalResults[0]).show()
-    $(conditionalResults[1]).hide()
-  } else if (no) {
-    $(conditionalResults[0]).hide()
-    $(conditionalResults[1]).show()
+  var i = 0,
+      currentSwitch = items.first().children('.switch').first(),
+      conditionalResults = currentSwitch.siblings('.switch-result'),
+      yes = currentSwitch.children('input:checked[id*="-yes"]')[0],
+      no = currentSwitch.children('input:checked[id*="-no"]')[0]
+      
+  console.log(yes)
+  console.log(no)
+  
+  while(yes || no) {
+    if(yes) {
+      $(conditionalResults[0]).show()
+      $(conditionalResults[1]).hide()
+    } else if (no) {
+      $(conditionalResults[0]).hide()
+      $(conditionalResults[1]).show()
+    }
+    if($(items[i+1]).hasClass('hidden')) {
+      $(items[i+1]).removeClass('hidden')
+    }
+    else break
+    
+    i++
+    currentSwitch = $(items[i]).children('.switch').first()
+    conditionalResults = currentSwitch.siblings('.switch-result')
+    yes = currentSwitch.children('input:checked[id*="-yes"]')
+    no = currentSwitch.children('input:checked[id*="-no"]')
   }
-  if($(items[i+1]).hasClass('hidden'))
-    $(items[i+1]).removeClass('hidden')
     
 })
